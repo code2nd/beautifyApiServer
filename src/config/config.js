4b065d1e39449494309026f6047122c12c36cb5e
Jalamy <wy20150905zc@163.com>
2020-08-26 17:00:27 +0800

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -11,25 +11,25 @@ module.exports = {
   port: 3005,
   database: {
     dbName: 'beautifyApi',
-    host: '47.107.229.37',
+    host: 'your host',
     port: 3306,
     user: 'root',
-    password: 'pws921219'
+    password: 'your password'
   },
   security: {
-    secretKey: 'dsa5151d*3%&@fGjcs)&egfr#grgb5', // jwt令牌加密随机字符串，越复杂无规律越好
+    secretKey: 'your secretKey', // jwt令牌加密随机字符串，越复杂无规律越好
     expiresIn: 60*60*24       // 令牌的过期时间，60*60代表的是一个小时
   },
   redis: {
-    port: 6379,
+    port: your port,
     host: "127.0.0.1",
-    password: "jalamy123456",
+    password: "your password",
     db: 0
   },
   github: {
     request_token_url: 'https://github.com/login/oauth/access_token',
     client_id,
-    client_secret: '787d7b14585faa0d04f5b985224266c8ef3e2013'
+    client_secret: 'your client_secret'
   },
   GITHUB_OAUTH_URL,
   OAUTH_URL: `${GITHUB_OAUTH_URL}?client_id=${client_id}&scope=${SCOPE}`,
